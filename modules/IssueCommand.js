b52a5c6a49792bc6c47e9df31482f0a3a1df387c
Neil Seward <neil.seward@outlook.com>
2015-02-07 00:08:44 -0500

    Finished Issue and added check to Output.txt

--- modules/IssueCommand.js
+++ modules/IssueCommand.js
@@ -1,6 +1,11 @@
 module.exports = (function () {
     function issueCommand(trainID, command) {
         var fs = require('fs');
+        if(!fs.existsSync('Output.txt'))
+        {
+            fs.writeFileSync("Output.txt","TrainID          Command");
+            console.log("Output.txt was created");
+        }
         var line = "\n"+trainID +"             "+ command;//adding line together
         fs.appendFileSync("Output.txt", line);
     }
