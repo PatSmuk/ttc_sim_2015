ca72867f236a2c99733d7ad6f457b73b59f8c2b6
unknown <100496078@UOSL14-90266PP.oncampus.local>
2015-02-06 21:56:14 -0500

    Initial commit

---
+++ views/app.jsx
@@ -0,0 +1,12 @@
+module.exports = (function () {
+
+    var React = require('react/addons');
+
+    var App = React.createClass({
+        render: function () {
+            return <h1>If you can read this, it's working!</h1>;
+        }
+    });
+
+    return App;
+})();
